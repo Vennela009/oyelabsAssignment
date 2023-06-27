9bc8f3aa3cd2baffcfaf10472db42da40509ae1e
Vennela009 <122536843+Vennela009@users.noreply.github.com>
2023-06-27 21:36:37 +0530

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,6 +1,15 @@
-const person = {};
-const student = {};
+const person = {
+  id :2,
+  gender:"mail"
+};
+
+const student = {
+  name:"ravi:",
+  email:"ravi11@yopmail.com"
+};
+
 const newObject = {
   ...person,...student
 };
 
+console.log(newObject);
