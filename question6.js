30cdfaf0086d5609a72c54e116950c34ef01733c
Vennela009 <122536843+Vennela009@users.noreply.github.com>
2023-06-27 22:17:18 +0530

    Update question6.js

--- question6.js
+++ question6.js
@@ -1,9 +1,8 @@
-
-var missingNumber = function(nums){
-  let n = nums.length +1 ; 
-  let total = (n *(n-1))/2;
-  for (let num of nums){
+var missingNumber = function (nums) {
+  let n = nums.length + 1;
+  let total = (n * (n - 1)) / 2;
+  for (let num of nums) {
     total -= num;
-  };
+  }
   return total;
 };
